9059776f747a8a9e499ff7a75041406fc5d68c28
Ayesha <68371207+akhalil95@users.noreply.github.com>
2024-04-05 18:27:56 -0600

    lab 5

--- App.jsx
+++ App.jsx
@@ -1,36 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import AboutScreen from './screens/AboutScreen';
 
-import React, {useState} from 'react';
-import {StyleSheet, SafeAreaView} from 'react-native';
-import ToDoList from './components/ToDoList';
-import ToDoForm from './components/ToDoForm';
+const Stack = createNativeStackNavigator();
 
 function App() {
-  const [tasks, setTasks] = useState(['Do laundry', 'Go to gym', 'Walk dog']);
-
-  const handleAddTask = task => {
-    setTasks([...tasks, task]);
-  };
-
   return (
-    <SafeAreaView style={styles.container}>
-      <ToDoList tasks={tasks} />
-      <ToDoForm addTask={handleAddTask} />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default App;
